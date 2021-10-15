fc57949eae0609638066f58faea2b063253de3b0
Raad Kamal <kammd1702@learning.icmanitoba.ca>
2021-10-15 08:21:42 -0500

    Blockchain

---
+++ Blockchain.py
@@ -0,0 +1,32 @@
+import time
+import Block
+
+
+class Blockchain:
+
+    difficulty=2
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
+
+
+    def proof_of_work(self, block, difficulty):
+        block.nonce = 0
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+        return computed_hash
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import uuid
 from _sha256 import sha256
 from datetime import datetime
 
+
 class Transaction:
-    hash_of_transaction=0
-    sender_addressess="" #type address
-    reciever_addressess="" #type address
+    hash_of_transaction = 0
+    sender_addressess = "" #type address
+    receiver_addressees = "" #type address
     time_stamp_of_linking = 0 #UNIXTIMECODE
+    ID=""
 
 
-    def __init__(self,hash_of_tran,sender_add,reciever_addr):
+    def __init__(self,hash_of_tran,sender_add,reciever_addr,id):
         self.hash_of_transaction=hash_of_tran
         self.sender_addressess=sender_add
-        self.reciever_addressess=reciever_addr
+        self.receiver_addressees=reciever_addr
+        ID=id
 
 
 
@@ -28,63 +32,6 @@ class Transaction:
    # def input_of_current_transaction_address(self,previous_transaction):
 
 
-class Block:
-    blockheight=0
-    previous_blockhash=0
-    nonce=0
-    timestamp = 0
-    transactionlist=[]
-
-
-    def __init__(self,nonce,previous_blockhash, hash):
-        self.nonce=nonce
-        self.previous_blockhash=previous_blockhash
-        self.hash=hash
-        self.timestamp = datetime.now()
-
-    def create_new_transaction(self,amount,sender,receiver):
-        new_transaction= Transaction(amount,sender,receiver)
-        self.transactionlist.append(new_transaction)
-
-
-class Blockchain:
-    chain=[]
-    Blockchain_size=0;
-    timestamp=0
-
-    def create_new_block(self,nonce_x,previous_blockhash_y,hash_z):
-        newblock= Block(nonce_x,previous_blockhash_y,hash_z)
-        self.Blockchain_size+=1
-        self.chain.append(newblock)
-
-
-
-    def getLastBlock(self)-> Block:
-        return self.chain[self.Blockchain_size]
-
-    def getCorrectBlock(self,Blockhash):
-        correctblock=0
-        for block in self.chain:
-            if(block.hash==Blockhash): correctblock=block
-        return correctblock
-
-
-    def Create_New_Trsansaction(self,Amount,sender,reciver) -> None :
-        return self.getLastBlock().create_new_transaction(Amount,sender,reciver)
-
-    def hash_block(self,previous_hash,currentData,nonce):
-        datastring=previous_hash+str(nonce)+str(currentData)+""
-        hash=sha256(datastring)
-        return hash
-
-    def proof_of_work(self,prevousblockhash,currentblockdata):
-        nonce=0
-        hash=self.hash_block(prevousblockhash,currentblockdata,nonce)
-        while(hash[0.4]!="0000"):
-            nonce++1
-            hash=self.hash_block(prevousblockhash,currentblockdata,nonce)
-
-        return nonce
 
 
 
