fdebe54b2f0ae24753f9ad79cd42940bc65de466
Raad Kamal <kammd1702@learning.icmanitoba.ca>
2021-10-15 13:43:58 -0500

    Blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -21,8 +21,6 @@ class Blockchain:
         return self.chain[-1]
 
 
-
-
     def proof_of_work(self, block, difficulty):
         block.nonce = 0
         computed_hash = block.compute_hash()
@@ -30,3 +28,19 @@ class Blockchain:
             block.nonce += 1
             computed_hash = block.compute_hash()
         return computed_hash
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block()
+        if previous_hash != block.previous_hash:
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        self.chain.append(block)
+        return True
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
