0eaa223f6fabe68707c3b410c839a5dff03a195d
youn <youns@myumanitoba.ca>
2021-12-17 12:34:03 -0600

    edit load_darnet and load_network files

--- load_darknet.py
+++ load_darknet.py
@@ -4,92 +4,129 @@ import gc
 import numpy as np
 import pandas as pd
 import datetime as dt
-
+import time
 
 intermediate_dir = "data/intermediate_data/"
 month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+year = 2015
 bad_times = {}
 bad_prices = {}
 
-
-# Convert the given timestamp into a readable format
+# Convert the given timestamp into a readable format as example: 2015/07/27 08:58
 def convertTime(input_time):
-    return dt.datetime.utcfromtimestamp(input_time).strftime("%Y/%m/%d %H:%M")
+	return dt.datetime.utcfromtimestamp(input_time).strftime("%Y/%m/%d %H:%M")
 
 
 # this method reads prices and times of darknet address in a specific darknet file
+'''
+It takes the path of a darknet file, converts it into a pandas dataframe and reads all rows.
+For each row, it parses the timestamp and extract year, month and date.
+Then, it adds the timestamp and price of the row into the dictionary of the corresponding day
+The purpose is to store all addresses of this file into a dictionary against transaction dates 
+'''
 def readDarknetData(file_path):
-    market_data = pd.read_csv(file_path)
-    # separating arrays of prices and times
-    addr_prices = market_data['price']
-    addr_times = market_data['add_time']
-    market_data = None
+	# read the darknet file
+	try:
+		market_data = pd.read_csv(file_path)
+	except Exception as e:
+		return
 
-    # iterating over all darknet addresses
-    for i in range(len(addr_times)):
-        # extracting month and day of the darknet address
-        cur_tx_time = convertTime(int(addr_times[i]))
-        month = int(cur_tx_time[5:7])
-        day = int(cur_tx_time[8:10])
+	# separating arrays of prices and times
+	addr_prices = market_data['price']
+	addr_times = market_data['add_time']
+	market_data = None
 
-        # storing address times and prices into dictionaries of corresponding day
-        bad_times[str(month) + "-" + str(day)].append(addr_times[i])
-        bad_prices[str(month) + "-" + str(day)].append(addr_prices[i])
+	# iterating over all rows in this file
+	for i in range(len(addr_times)):
+		# convert the timestamp into year-month-day hour:minute format
+		cur_tx_time = convertTime(int(addr_times[i]))
 
-    # clearing unnecessary variables
-    addr_times = None
-    addr_prices = None
+		# extracting year, month and day of the darknet address
+		yr = int(cur_tx_time[0:4])
+		month = int(cur_tx_time[5:7])
+		day = int(cur_tx_time[8:10])
 
+		# if current address is not for the year we are working, skip
+		if yr != year:
+			continue
+
+		# storing address times and prices into dictionaries of corresponding day
+		bad_times[str(month) + "-" + str(day)].append(addr_times[i])
+		bad_prices[str(month) + "-" + str(day)].append(addr_prices[i])
+
+	# clearing unnecessary variables
+	addr_times = None
+	addr_prices = None
+	
+	
 
 if __name__ == '__main__':
-    print("Reading Darknet market data...")
+	'''
+	Here, we store time stamps and prices of all darknet files into two files: one for times which we call
+	bad times, and another for prices which we call bad prices
+	'''
 
-    # initializing data holder dictionaries
-    for i in range(12):
-        for j in range(1, month_days[i] + 1):
-            bad_times[str(i + 1) + "-" + str(j)] = []
-            bad_prices[str(i + 1) + "-" + str(j)] = []
+	print("Reading Darknet market data...")
+	t_start = time.time()
 
-    # reading darket data of all folders
-    folders = os.listdir('data/grams/')
+	# initializing data holder dictionaries for 12 months
+	for i in range(12):
+		for j in range(1, month_days[i] + 1):
+			bad_times[str(i + 1) + "-" + str(j)] = []
+			bad_prices[str(i + 1) + "-" + str(j)] = []
 
-    for folder in folders:
-        files = os.listdir('data/grams/' + folder)
+	# reading darket data of all folders
+	folders = os.listdir('data/grams/')
 
-        # Collecting data from  Agora files
-        if 'Agora.csv' not in files:
-            continue
-        readDarknetData('data/grams/' + folder + "/" + 'Agora.csv')
-        gc.collect()
+	for folder in folders:
+		# get a list of all files within each folder
+		files = os.listdir('data/grams/' + folder)
 
-    # after loading folder data into dictionaries, save dictionaries into files
-    # iterate month by month
-    for i in range(12):
-        # initializing folder names
-        month = i + 1
-        month_dir = intermediate_dir + "month_" + str(month)
-        bad_times_dir = month_dir + "/bad_times"
-        bad_prices_dir = month_dir + "/bad_prices"
+		#uncomment below code if you want want to work on a specific file such as Agora.csv
+		'''if 'Agora.csv' not in files:
+			continue
+		readDarknetData('data/grams/' + folder + "/" + 'Agora.csv')
+		gc.collect()'''
 
-        # check if folder exists. If exists delete and create new
-        if os.path.exists(bad_times_dir) and os.path.isdir(bad_times_dir):
-            shutil.rmtree(bad_times_dir)
-        if os.path.exists(bad_prices_dir) and os.path.isdir(bad_prices_dir):
-            shutil.rmtree(bad_prices_dir)
+		#comment below code if you want want to work on a specific file such as Agora.csv
+		# read all files under current folder
+		for fname in files:
+			# call the method we initialized earlier, it stores info of this file into dictionaries
+			readDarknetData('data/grams/' + folder + "/" + fname)
+			# clear unnecessary caches
+			gc.collect()
 
-        os.mkdir(bad_times_dir)
-        os.mkdir(bad_prices_dir)
+	# after loading folder data into dictionaries, save dictionaries into files
+	# iterate month by month from 1 to 12
+	for i in range(12):
+		# initializing folder names of bad times and bad prices for each month
+		month = i + 1
+		month_dir = intermediate_dir + "month_" + str(month)
+		bad_times_dir = month_dir + "/bad_times"
+		bad_prices_dir = month_dir + "/bad_prices"
 
-        # iterate day by day
-        for day in range(1, month_days[i] + 1):
-            if len(bad_times[str(month) + "-" + str(day)]) == 0:
-                continue
-            with open(bad_times_dir + "/bad_times_" + str(day) + ".npy", "wb") as f:
-                np.save(f, np.array(bad_times[str(month) + "-" + str(day)]))
-                bad_times[str(month) + "-" + str(day)] = None
+		# check if folder exists. If exists delete them
+		if os.path.exists(bad_times_dir) and os.path.isdir(bad_times_dir):
+			shutil.rmtree(bad_times_dir)
+		if os.path.exists(bad_prices_dir) and os.path.isdir(bad_prices_dir):
+			shutil.rmtree(bad_prices_dir)
 
-            with open(bad_prices_dir + "/bad_prices_" + str(day) + ".npy", "wb") as f:
-                np.save(f, np.array(bad_prices[str(month) + "-" + str(day)]))
-                bad_prices[str(month) + "-" + str(day)] = None
+		# create folders
+		os.mkdir(bad_times_dir)
+		os.mkdir(bad_prices_dir)
 
-    print("Darknet data reading finished.")
+		# iterate day by day and save the data of each day as numpy array files
+		for day in range(1, month_days[i] + 1):
+			if len(bad_times[str(month) + "-" + str(day)]) == 0:
+				continue
+			with open(bad_times_dir + "/bad_times_" + str(day) + ".npy", "wb") as f:
+				np.save(f, np.array(bad_times[str(month) + "-" + str(day)]))
+				bad_times[str(month) + "-" + str(day)] = None
+
+			with open(bad_prices_dir + "/bad_prices_" + str(day) + ".npy", "wb") as f:
+				np.save(f, np.array(bad_prices[str(month) + "-" + str(day)]))
+				bad_prices[str(month) + "-" + str(day)] = None
+
+	print("Darknet data reading finished.")
+	t_end = time.time()
+	print("Darknet data loading time:", str(t_end - t_start), "seconds")
--- load_network.py
+++ load_network.py
@@ -2,6 +2,7 @@ import os
 import gc
 import datetime as dt
 import numpy as np
+import time
 
 year = 2015
 data_dir = "data/edges" + str(year) + "/"
@@ -9,121 +10,146 @@ intermediate_dir = "data/intermediate_data/"
 month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 output_files = []
 
-
-# automatically generate names of output files
+# automatically generate names of output files under the year defined at top
 def generateOutputFileNames():
-    for i in range(12):
-        output_files.append("outputs" + str(year) + "_" + str(i + 1) + ".txt")
+	for i in range(12):
+		output_files.append("outputs" + str(year) + "_" + str(i + 1) + ".txt")
 
-
-# Convert the given timestamp into a readable format
+# Convert the given timestamp into a readable format as example: 2015/07/27 08:58
 def convertTime(input_time):
-    return dt.datetime.utcfromtimestamp(input_time).strftime("%Y/%m/%d %H:%M")
+	return dt.datetime.utcfromtimestamp(input_time).strftime("%Y/%m/%d %H:%M")
 
 
 # this method reads data in a month by month basis
+'''
+This method performs all necessary operations for loading data and linking/processing the data
+It takes as parameter a month number, then read and process data of that month
+It stores address info of that month in a day by day basis
+Address info includes transaction timestamps, price of an address, hash of an address,
+no of output address of the corresponding transaction, average price of all output addresses
+of the corresponding transaction. It stores these six data into six numpy arrays in a day by day basis under a folder
+for this month
+'''
 def readNetworkData(month):
-    month_dir = intermediate_dir + "month_" + str(month + 1)
-    os.mkdir(month_dir)
+	month_dir = intermediate_dir + "month_" + str(month + 1)
+	os.mkdir(month_dir)
 
-    # initialize empty data dictionaries
-    tx_times = {}
-    addr_counts = {}
-    avg_prices = {}
-    addr_prices = {}
-    addr_tx_index = {}
-    addr_hashes = {}
+	# initialize empty data dictionaries
+	tx_times = {} # dictionary for transaction timestamps
+	addr_counts = {} # dictionary for no of output address of the corresponding transaction
+	avg_prices = {} # dictionary for average price of all output addresses of a transaction
+	addr_prices = {} # dictionary for price of current address
+	addr_tx_index = {} # dictionary for index of transaction of this address
+	addr_hashes = {} # dictionary for has address of this address
 
-    # initialize each day of data dictionary with empty lists
-    for day in range(1, month_days[month] + 1):
-        tx_times[day] = []
-        addr_counts[day] = []
-        avg_prices[day] = []
-        addr_prices[day] = []
-        addr_tx_index[day] = []
-        addr_hashes[day] = []
+	# initialize each day of data dictionary with empty lists
+	for day in range(1, month_days[month] + 1):
+		tx_times[day] = []
+		addr_counts[day] = []
+		avg_prices[day] = []
+		addr_prices[day] = []
+		addr_tx_index[day] = []
+		addr_hashes[day] = []
 
-    # Open output file
-    outputData = open(data_dir + output_files[month])
+	# Open output file
+	outputData = open(data_dir + output_files[month])
 
-    # iterate every line of output file
-    for line in outputData:
-        # spliting words in a line
-        values = line.split("\t")
-        cur_tx_time = convertTime(int(values[0]))
-        day = int(cur_tx_time[8:10])
+	# iterate every line of output file
+	for line in outputData:
+		# spliting words in a line
+		values = line.split("\t")
+		# conver the timestamp
+		cur_tx_time = convertTime(int(values[0]))
+		day = int(cur_tx_time[8:10])
 
-        # setting data to the appropriate dictionary
-        temp_total_price = 0
-        temp_addr_count = 0
-        for i in range(2, len(values), 2):
-            addr_tx_index[day].append(len(tx_times[day]))
-            addr_hashes[day].append(values[i])
-            addr_prices[day].append(int(values[i + 1]))
-            temp_addr_count += 1
-            temp_total_price += int(values[i + 1])
-        if temp_addr_count > 0:
-            tx_times[day].append(values[0])
-            addr_counts[day].append(temp_addr_count)
-            avg_prices[day].append(temp_total_price // temp_addr_count)
+		# setting data to the appropriate dictionary
+		temp_total_price = 0
+		temp_addr_count = 0
+		for i in range(2, len(values), 2):
+			# adding info of this row/address into dictionaries
+			addr_tx_index[day].append(len(tx_times[day]))
+			addr_hashes[day].append(values[i])
+			addr_prices[day].append(int(values[i + 1]))
+			temp_addr_count += 1
+			temp_total_price += int(values[i + 1])
+		# this address has neighbors/output address of same transaction
+		if temp_addr_count > 0:
+			tx_times[day].append(values[0])
+			addr_counts[day].append(temp_addr_count)
+			avg_prices[day].append(temp_total_price//temp_addr_count)
 
-    # initialize data directory names
-    tx_times_dir = month_dir + "/tx_times"
-    addr_counts_dir = month_dir + "/addr_counts"
-    avg_prices_dir = month_dir + "/avg_prices"
-    addr_prices_dir = month_dir + "/addr_prices"
-    addr_tx_index_dir = month_dir + "/addr_tx_index"
-    addr_hashes_dir = month_dir + "/addr_hashes"
-    os.mkdir(tx_times_dir)
-    os.mkdir(addr_counts_dir)
-    os.mkdir(avg_prices_dir)
-    os.mkdir(addr_prices_dir)
-    os.mkdir(addr_tx_index_dir)
-    os.mkdir(addr_hashes_dir)
+	# initialize data directory names to save numpy arrays
+	tx_times_dir = month_dir + "/tx_times"
+	addr_counts_dir = month_dir + "/addr_counts"
+	avg_prices_dir = month_dir + "/avg_prices"
+	addr_prices_dir = month_dir + "/addr_prices"
+	addr_tx_index_dir = month_dir + "/addr_tx_index"
+	addr_hashes_dir = month_dir + "/addr_hashes"
 
-    # saving data into files in a day by day basis
-    for day in range(1, month_days[month] + 1):
-        with open(tx_times_dir + "/tx_times_" + str(day) + ".npy", "wb") as f:
-            np.save(f, np.array(tx_times[day]))
-            tx_times[day] = None
+	# create the directories
+	os.mkdir(tx_times_dir)
+	os.mkdir(addr_counts_dir)
+	os.mkdir(avg_prices_dir)
+	os.mkdir(addr_prices_dir)
+	os.mkdir(addr_tx_index_dir)
+	os.mkdir(addr_hashes_dir)
 
-        with open(addr_counts_dir + "/addr_counts_" + str(day) + ".npy", "wb") as f:
-            np.save(f, np.array(addr_counts[day]))
-            addr_counts[day] = None
+	# saving data into files as numpy array in a day by day basis
+	for day in range(1, month_days[month] + 1):
+		with open(tx_times_dir + "/tx_times_" + str(day) + ".npy", "wb") as f:
+			np.save(f, np.array(tx_times[day]))
+			tx_times[day] = None
 
-        with open(avg_prices_dir + "/avg_prices_" + str(day) + ".npy", "wb") as f:
-            np.save(f, np.array(avg_prices[day]))
-            avg_prices[day] = None
+		with open(addr_counts_dir + "/addr_counts_" + str(day) + ".npy", "wb") as f:
+			np.save(f, np.array(addr_counts[day]))
+			addr_counts[day] = None
 
-        with open(addr_prices_dir + "/addr_prices_" + str(day) + ".npy", "wb") as f:
-            np.save(f, np.array(addr_prices[day]))
-            addr_prices[day] = None
+		with open(avg_prices_dir + "/avg_prices_" + str(day) + ".npy", "wb") as f:
+			np.save(f, np.array(avg_prices[day]))
+			avg_prices[day] = None
 
-        with open(addr_tx_index_dir + "/addr_tx_index_" + str(day) + ".npy", "wb") as f:
-            np.save(f, np.array(addr_tx_index[day]))
-            addr_tx_index[day] = None
+		with open(addr_prices_dir + "/addr_prices_" + str(day) + ".npy", "wb") as f:
+			np.save(f, np.array(addr_prices[day]))
+			addr_prices[day] = None
 
-        with open(addr_hashes_dir + "/addr_hashes_" + str(day) + ".npy", "wb") as f:
-            np.save(f, np.array(addr_hashes[day]))
-            addr_hashes[day] = None
+		with open(addr_tx_index_dir + "/addr_tx_index_" + str(day) + ".npy", "wb") as f:
+			np.save(f, np.array(addr_tx_index[day]))
+			addr_tx_index[day] = None
 
-    # clearing dictionaries as inside data no longer required
-    tx_times.clear()
-    addr_counts.clear()
-    avg_prices.clear()
-    addr_prices.clear()
-    addr_tx_index.clear()
-    addr_hashes.clear()
+		with open(addr_hashes_dir + "/addr_hashes_" + str(day) + ".npy", "wb") as f:
+			np.save(f, np.array(addr_hashes[day]))
+			addr_hashes[day] = None
+
+	# clearing dictionaries as inside data no longer required
+	tx_times.clear()
+	addr_counts.clear()
+	avg_prices.clear()
+	addr_prices.clear()
+	addr_tx_index.clear()
+	addr_hashes.clear()
 
 
 if __name__ == '__main__':
-    generateOutputFileNames()
+	'''
+	we do not report data loading time and linking time separately.
+	We load a month data and perform necessary linking, then proceed to next month
+	So our data loading time involves both data loading and linking
+	'''
 
-    # load data and generate network in a month basis
-    for i in range(12):
-        print("Loading month " + str(i + 1) + " data ...")
-        readNetworkData(i)
-        # clearing cach after reading and saving each month data
-        gc.collect()
+	t_start = time.time()
+	# call the method which generate all file names of this year
+	generateOutputFileNames()
 
-    print("Network data loading finished.")
+	# load data and generate network in a month basis
+	for i in range(12):
+		print("Loading month " + str(i + 1) + " data ...")
+		# call the method we initialized to parse and store info of a month
+		readNetworkData(i)
+		# clearing cach after reading and saving each month data
+		gc.collect()
+
+	print("Network data loading finished.")
+	t_end = time.time()
+	print("Graph data loading and linking time:", str(t_end - t_start), "seconds")
+
+
