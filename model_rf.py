f6ff57f2310fd0bce7900c984a39a6da309bf3ad
ashraf04066 <92474643+ashraf04066@users.noreply.github.com>
2021-12-19 21:11:00 -0600

    Update model_rf.py
    Getting more accurate result

--- model_rf.py
+++ model_rf.py
@@ -8,16 +8,17 @@ from sklearn.metrics import f1_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 
-
+# this method loads the feature dataset and convert into 80% vs 20% train and test dataset
 def buildModel():
-	
-	n_estimators = [100, 150, 200, 250]
-	max_depth = [int(x) for x in np.linspace(60, 100, num = 20)]
+	# set parameters range for random search of hyper parameters
+	n_estimators = [5, 10, 15, 20]
+	max_depth = [int(x) for x in np.linspace(4, 17, num = 4)]
 	min_samples_split = [2, 5, 10]
-	min_samples_leaf = [1, 2, 4]
+	min_samples_leaf = [2, 4]
 	bootstrap = [True, False]
 	criterion = ['entropy']
 
+	#create the radom grid
 	random_grid = {'n_estimators': n_estimators,
 	'max_depth': max_depth,
 	'min_samples_split': min_samples_split,
@@ -25,22 +26,23 @@ def buildModel():
 	'bootstrap': bootstrap,
 	'criterion': criterion}
 
+	# create random forest model class
 	rf = RandomForestClassifier()
 	rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, cv = 2, random_state = 42, n_jobs = 5)
 
 	return rf_random
 
-
+# this method trains the model on the parameters for random search and returns the best model
 def trainModel(modelRf, trainX, trainY):
-	
 	modelRf.fit(trainX, trainY)
 	print("Best Model Parameters:", modelRf.best_params_)
 	return modelRf.best_estimator_
 
-
+# evaluate the best random forest model with unseen data
 def testModel(modelRf, testX, testY):
 	predictions = modelRf.predict(testX)
 
+	# save the predictions of random forest model
 	with open('data/model_data/predictions_rf.npy', 'wb') as f:
 		np.save(f, np.array(predictions))
 
@@ -51,25 +53,24 @@ def testModel(modelRf, testX, testY):
 
 
 if __name__ == '__main__':
-		# load previously processed train and test data
+
+	# load previously processed train and test data
 	trainX = np.load('data/model_data/trainX.npy')
 	trainY = np.load('data/model_data/trainY.npy')
 	testX = np.load('data/model_data/testX.npy')
 	testY = np.load('data/model_data/testY.npy')
 
-
+	# build the random forest classifier model
 	rf_random = buildModel()
 
+	# train the model and get the one with best parameters
 	start = time.time()
 	modelRf = trainModel(rf_random, trainX, trainY)
 	end = time.time()
 	print("Total training time for random forest:", (end - start), "seconds")
 
-
+	# evaluate the model
 	start = time.time()
 	testModel(modelRf, testX, testY)
 	end = time.time()
 	print("Total evaluation time for random forest:", (end - start), "seconds")
-
-
-
